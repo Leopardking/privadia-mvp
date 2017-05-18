bf2d4b0b3e19d6a91e7a85e1d32d5a62f3a4ecf3
Samuel Rabatin <nessaj55@yandex.com>
2017-05-18 10:38:03 +0200

    Add semantic UI plugin

---
+++ assets/js/init/initSemanticSelect.d.ts
@@ -0,0 +1,2 @@
+declare function initSemanticSelect():void;
+export = initSemanticSelect;
---
+++ assets/js/init/initSemanticSelect.js
@@ -0,0 +1,9 @@
+if ('undefined' !== typeof module) {
+
+    module.exports = function initSemanticSelect(){
+        // Sliders for demo purpose in refine cards section
+        // Sliders for demo purpose
+
+        //console.log($(".semantic-drop-down").dropdown());
+    }
+}
