02590f0d5be041993e05870c2758bbddd76ab501
Alexandr Loginov <trashloginov@gmail.com>
2017-06-01 16:00:44 +0300

    redesigned sidebar

--- app/components/sidebar/sidebar-routes.config.js
+++ app/components/sidebar/sidebar-routes.config.js
@@ -1,10 +1,10 @@
 "use strict";
 var sidebar_metadata_1 = require('./sidebar.metadata');
 exports.ROUTES = [
-    { path: 'home', title: 'Dashboard', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons' },
-    { path: 'booking', title: 'Bookings', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons' },
-    { path: 'properties', title: 'Properties', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons' },
-    { path: 'home', title: 'Notifications', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons' },
-    { path: 'home', title: 'Users', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons' },
+    { path: 'home', title: 'Dashboard', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons', iconName: 'dashboard' },
+    { path: 'booking', title: 'Bookings', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons', iconName: 'confirmation_number' },
+    { path: 'properties', title: 'Properties', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons', iconName: 'store' },
+    { path: 'home', title: 'Notifications', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons', iconName: 'notifications' },
+    { path: 'home', title: 'Users', menuType: sidebar_metadata_1.MenuType.LEFT, icon: 'material-icons', iconName: 'group' },
 ];
 //# sourceMappingURL=sidebar-routes.config.js.map
