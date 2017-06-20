61a6260f14beaed89c07603ac7b10db529f50084
Alexandr Loginov <trashloginov@gmail.com>
2017-06-20 14:26:54 +0300

    without login

--- app/pages/dashboard.routes.js
+++ app/pages/dashboard.routes.js
@@ -1,14 +1,13 @@
 "use strict";
-var auth_guard_1 = require("../providers/common/auth.guard");
 var home_component_1 = require('./home/home.component');
 var villa_component_1 = require('../components/villa/villa.component');
 var metafilter_component_1 = require('../components/metafilter/metafilter.component');
 exports.MODULE_ROUTES = [
     //{ path: '', pathMatch: 'full' },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: home_component_1.HomeComponent, canActivate: [auth_guard_1.AuthGuard] },
-    { path: 'booking', loadChildren: 'app/pages/booking/booking.module#BookingModule', canActivate: [auth_guard_1.AuthGuard] },
-    { path: 'properties', loadChildren: 'app/pages/properties/properties.module#PropertiesModule', canActivate: [auth_guard_1.AuthGuard] },
+    { path: 'home', component: home_component_1.HomeComponent /*, canActivate: [AuthGuard]*/ },
+    { path: 'booking', loadChildren: 'app/pages/booking/booking.module#BookingModule' /*, canActivate: [AuthGuard]*/ },
+    { path: 'properties', loadChildren: 'app/pages/properties/properties.module#PropertiesModule' /*, canActivate: [AuthGuard]*/ },
 ];
 exports.MODULE_COMPONENTS = [
     home_component_1.HomeComponent,
--- app/pages/dashboard.routes.ts
+++ app/pages/dashboard.routes.ts
@@ -8,9 +8,9 @@ import { MetafilterComponent } from '../components/metafilter/metafilter.compone
 export const MODULE_ROUTES: Route[] =[
 	//{ path: '', pathMatch: 'full' },
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'booking', loadChildren: 'app/pages/booking/booking.module#BookingModule', canActivate: [AuthGuard] },
-    { path: 'properties', loadChildren: 'app/pages/properties/properties.module#PropertiesModule', canActivate: [AuthGuard] },
+    { path: 'home', component: HomeComponent/*, canActivate: [AuthGuard]*/ },
+    { path: 'booking', loadChildren: 'app/pages/booking/booking.module#BookingModule'/*, canActivate: [AuthGuard]*/ },
+    { path: 'properties', loadChildren: 'app/pages/properties/properties.module#PropertiesModule'/*, canActivate: [AuthGuard]*/ },
 ];
 
 export const MODULE_COMPONENTS = [
