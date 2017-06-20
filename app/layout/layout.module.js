57af46835c4c9354ff65aa5f9b52fbe7bb125afa
Alexandr Loginov <trashloginov@gmail.com>
2017-06-20 17:18:02 +0300

    new layout component

---
+++ app/layout/layout.module.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var common_1 = require('@angular/common');
+//import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+//import { TranslateModule } from '@ngx-translate/core';
+var layout_routing_module_1 = require('./layout-routing.module');
+var layout_component_1 = require('./layout.component');
+var header_component_1 = require('../shared/components/header/header.component');
+var sidebar_component_1 = require('../shared/components/sidebar/sidebar.component');
+var LayoutModule = (function () {
+    function LayoutModule() {
+    }
+    LayoutModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                common_1.CommonModule,
+                //NgbDropdownModule.forRoot(),
+                layout_routing_module_1.LayoutRoutingModule,
+            ],
+            declarations: [
+                layout_component_1.LayoutComponent,
+                header_component_1.HeaderComponent,
+                sidebar_component_1.SidebarComponent,
+            ]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], LayoutModule);
+    return LayoutModule;
+}());
+exports.LayoutModule = LayoutModule;
+//# sourceMappingURL=layout.module.js.map
