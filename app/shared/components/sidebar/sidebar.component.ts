56776e812ccbbd72a870bb08c144f48b703d533f
Alexandr Loginov <trashloginov@gmail.com>
2017-06-20 17:41:38 +0300

    new sidebar component

---
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {MainService} from "../../../providers/homeservice";
+
+import { ROUTES } from './sidebar-routes.config';
+import { MenuType } from './sidebar.metadata';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-sidebar',
+    templateUrl: 'sidebar.component.html',
+    styleUrls: ['sidebar.component.css']
+})
+export class SidebarComponent implements OnInit{
+    public menuItems: any[];
+
+    constructor ( private mainService: MainService ) {
+
+    }
+
+    ngOnInit() {
+        $.getScript('../../assets/js/sidebar-moving-tab.js');
+        //$.getScript('../../assets/js/plugins/bootstrap-datetimepicker.js');
+
+        this.menuItems = ROUTES.filter(menuItem => menuItem.menuType !== MenuType.BRAND);
+    }
+
+    private menuClick() {
+        //$('.sidebar .sidebar-wrapper, .main-panel').scrollTop(0);
+        //$("dashboard-cmp .main-content").scrollTop(0);
+
+        //return true;
+    }
+}
