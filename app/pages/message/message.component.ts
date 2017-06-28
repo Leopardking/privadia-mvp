39e3f70bf28616ae5155df2114ec50e8ba7e9c23
Alexandr Loginov <trashloginov@gmail.com>
2017-06-28 15:18:02 +0300

    fixed message table

--- app/pages/message/message.component.ts
+++ app/pages/message/message.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DashboardService } from '../../providers/dashboard/dashboard.service';
-
-import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 declare const $:any;
-declare const moment:any;
 
 @Component({
     moduleId: module.id,
@@ -14,32 +10,18 @@ declare const moment:any;
 })
 
 export class MessageComponent implements OnInit{
-    public enquiryForm = new FormGroup ({});
-    public proposalForm = new FormGroup ({});
-
-    constructor ( private dashboardService: DashboardService, private builder: FormBuilder) { }
+    constructor ( ) { }
 
     ngOnInit(){
         $('.sidebar .sidebar-wrapper, .main-panel').scrollTop(0);
-        $('.modal').appendTo("body");
+
         setTimeout(()=> {
-            $('.modal-content').perfectScrollbar();
-        },100)
-
-        this.enquiryForm = this.builder.group({
-            FirstName: new FormControl(),
-            LastName: new FormControl(),
-            CheckIn: new FormControl(moment().format('MM/DD/YYYY')),
-            CheckOut: new FormControl(moment().add(1, 'day').format('MM/DD/YYYY')),
-            Message: new FormControl(),
-        });
-
-        this.proposalForm = this.builder.group({
-            ClientName: new FormControl('Johnathan Robinson'),
-            CheckIn: new FormControl(moment().format('MM/DD/YYYY')),
-            CheckOut: new FormControl(moment().add(1, 'day').format('MM/DD/YYYY')),
-            RentalPrice: new FormControl(1200),
-            BookingPrice: new FormControl(1495),
-        })
+            let dataTableQuery: any = $('#datatables');
+            const tableWidget = dataTableQuery.DataTable({
+                bLengthChange: false,
+                ordering: false,
+                info: false,
+            });
+        }, 10)
     }
 }
