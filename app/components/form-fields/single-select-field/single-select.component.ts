0369c32135a487aeddc420bbf510f7e9613b007d
Alexandr Loginov <trashloginov@gmail.com>
2017-06-29 16:23:04 +0300

    added single select component

---
+++ app/components/form-fields/single-select-field/single-select.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit, Input, AfterViewInit} from '@angular/core';
+import {FormGroup, FormArray, FormControl, FormBuilder} from "@angular/forms";
+
+declare const $: any;
+import * as _ from "lodash";
+
+@Component({
+    moduleId: module.id,
+    selector: 'singleselect-field-cmp',
+    templateUrl: 'single-select.component.html',
+    styleUrls: [ 'single-select.component.css' ]
+})
+
+export class SingleSelectfieldComponent implements OnInit, AfterViewInit {
+    @Input('field') public field: FormGroup;
+    @Input('dataList') public dataList = [];
+    @Input('options') public options: any;
+
+    constructor ( ) {}
+
+    ngOnInit() {
+        const selectQuery = $(".custompicker");
+
+        selectQuery.selectpicker();
+    }
+
+    ngAfterViewInit() {
+        const selectQuery = $(".custompicker");
+        selectQuery.selectpicker('refresh');
+
+        selectQuery.on('show.bs.select', function (e) {
+            $('.dropdown-menu.inner').perfectScrollbar('update');
+        });
+    }
+}
