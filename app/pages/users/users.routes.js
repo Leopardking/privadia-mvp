2a5dae37ce0f4c20e53a7a6dd426bb33d31b0982
Alexandr Loginov <trashloginov@gmail.com>
2017-07-13 15:27:26 +0300

    refactoring

--- app/pages/users/users.routes.js
+++ app/pages/users/users.routes.js
@@ -1,9 +1,4 @@
 "use strict";
-var login_component_1 = require('./login/login.component');
-exports.MODULE_ROUTES = [
-    { path: 'login', component: login_component_1.LoginComponent },
-];
-exports.MODULE_COMPONENTS = [
-    login_component_1.LoginComponent,
-];
+exports.MODULE_ROUTES = [];
+exports.MODULE_COMPONENTS = [];
 //# sourceMappingURL=users.routes.js.map
--- app/pages/users/users.routes.ts
+++ app/pages/users/users.routes.ts
@@ -1,10 +1,7 @@
 import { Route, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
 export const MODULE_ROUTES: Route[] =[
-	{ path: 'login', component: LoginComponent },
 ]
 
 export const MODULE_COMPONENTS = [
-    LoginComponent,
 ]
