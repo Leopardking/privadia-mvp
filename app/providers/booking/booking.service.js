34f67e9a6b62218e242a94a1a8a861457698f2e1
Alexandr Loginov <trashloginov@gmail.com>
2017-07-19 13:28:20 +0300

    fixed booking service

--- app/providers/booking/booking.service.js
+++ app/providers/booking/booking.service.js
@@ -19,8 +19,8 @@ var BookingService = (function () {
     function BookingService(http, loginService) {
         this.http = http;
         this.loginService = loginService;
-        this.apiUrl = 'http://privadia-mvp-api-dev.azurewebsites.net';
-        // public apiUrl:string = 'http://privadia-mvp-api-2-dev.azurewebsites.net';
+        // private apiUrl: string = 'http://privadia-mvp-api-dev.azurewebsites.net';
+        this.apiUrl = 'http://privadia-mvp-api-2-dev.azurewebsites.net';
         this.token = localStorage.getItem('id_token');
         this.bookings = [];
     }
